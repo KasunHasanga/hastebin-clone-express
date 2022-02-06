748dae15494e120a545a6f6bb1a669814b5d95ec
KasunHasanga <kasunhasanga96@gmail.com>
2022-02-06 18:36:38 +0530

    add style to home page

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 app.set('view engine','ejs')
+app.use(express.static('public'))
 const port = 3000
 
 app.get('/', (req, res)=> {
-    const code =`
-Welcome to pastebin;
+    const code =`Welcome to pastebin;
 Sharing code is a good thing, and it should be _really_ easy to do it.
 A lot of times, I want to show you something I'm seeing - and that's where we
 use pastebins.
 
 Haste is the prettiest, easiest to use pastebin ever made.
-
     `
     res.render('code-display',{code})
 })
