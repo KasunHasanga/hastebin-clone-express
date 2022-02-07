4dc7e0f05c77ed9122b38595c05990f640dd7d22
KasunHasanga <kasunhasanga96@gmail.com>
2022-02-07 08:44:27 +0530

    save single code file

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-const express = require('express')
-const app = express()
-app.set('view engine','ejs')
-app.use(express.static('public'))
-const port = 3000
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+const port = 3000;
 
-app.get('/', (req, res)=> {
-    const code =`Welcome to pastebin;
+const Document = require("./models/Documents");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/hastenin", {});
+
+app.get("/", (req, res) => {
+  const code = `Welcome to pastebin;
 Sharing code is a good thing, and it should be _really_ easy to do it.
 A lot of times, I want to show you something I'm seeing - and that's where we
 use pastebins.
 
 Haste is the prettiest, easiest to use pastebin ever made.
-    `
-    res.render('code-display',{code})
+    `;
+  res.render("code-display", { code });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  // console.log(value);
+
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (error) {
+    res.render("new", { value });
+  }
+});
+
+app.get('/:id',async (req,res)=>{
+    const id =req.params.id;
+    try {
+        const document= await Document.findById(id)
+        res.render('code-display',{code:document.value});
+    } catch (error) {
+        res.redirect('/')
+    }
 })
 
-app.get('/new',(req,res)=>{
-    res.render("new");
-})
-
-app.listen(port)
+app.listen(port);
